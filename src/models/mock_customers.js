cf9511a98190c5cab36ad7e21f0014c4f76ebb08
Douglas Willian de Farias Brandão <douglaswfbrandao@gmail.com>
2018-10-29 10:45:29 -0300

    _a
    asda

--- src/models/mock_customers.js
+++ src/models/mock_customers.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
-    appKey: '7995317091',
-    appSecret: '6f16183fb1e833d72392d7369c7b0055',
+    appKey: '7995317987',
+    appSecret: '6f16183fb1e833d72392d7369c7b0077',
     customersName : 'MgServicosContabeis',
     customersgroup: 'MgContabilidade'
   }
